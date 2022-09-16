b1a71a59dd32967796d0b090009bd78bf3645415
Samyak Shah <samyakshah123@gmail.com>
2022-09-16 23:21:13 +0530

    Fixed bug regarding fetch by name

--- controllers/product.getProductByName.js
+++ controllers/product.getProductByName.js
@@ -5,7 +5,15 @@ exports.fetchProductsByName = async (req, res) => {
         const name = req.query.search
         const page = Number.parseInt(req.query.page)
         const limit = Number.parseInt(req.query.limit)
-        if (name !== 'undefined') {
+        const empty = req.query.empty
+        if ((name === 'undefined' || name.length === 0) && empty === 'required') {
+            const count = await Products.find().count()
+            const products = await Products
+                .find()
+                .skip((page - 1) * limit)
+                .limit(limit)
+            res.status(200).json({ message: 'Products fetched successfully', products, count })
+        } else if (name && name.length > 0) {
             const count = await Products.find({ name: { $regex: name, $options: 'i' } }).count()
             const products = await Products
                 .find({ name: { $regex: name, $options: 'i' } })
@@ -13,12 +21,7 @@ exports.fetchProductsByName = async (req, res) => {
                 .limit(limit)
             res.status(200).json({ message: 'Products fetched successfully', products, count })
         } else {
-            const count = await Products.find().count()
-            const products = await Products
-                .find()
-                .skip((page - 1) * limit)
-                .limit(limit)
-            res.status(200).json({ message: 'Products fetched successfully', products, count })
+            res.status(200).json({ message: 'Products fetched successfully', products: [], count: 0 })
         }
     } catch (err) {
         console.log(err)
