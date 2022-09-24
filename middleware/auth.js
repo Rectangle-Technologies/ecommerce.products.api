007504bcd4e7378a26fae8eb19869fefe6d1246a
MadMaxINDIAN <naman22khater@gmail.com>
2022-09-24 13:20:07 +0530

    bug resolved for auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/user");
+const user = require("../models/User");
 
 module.exports = async (req, res, next) => {
   const authHeader = req.get("Authorization");
---
+++ models/User.js
@@ -0,0 +1,84 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  contact: {
+    type: String,
+    required: true,
+  },
+  address: {
+    line1: {
+      type: String,
+      required: true
+    },
+    city: {
+      type: String,
+      required: true
+    },
+    state: {
+      type: String,
+      required: true
+    },
+    pincode: {
+      type: String,
+      required: true
+    }
+  },
+  orders: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Order",
+    },
+  ],
+  type: {
+    type: String,
+    enum: ["user", "admin", 'staff'],
+    default: "user",
+  },
+  cart: {
+    products: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+        size: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    total: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  wishlist: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+    },
+  ],
+});
+
+module.exports = mongoose.model("User", userSchema);
