6f4a42e37de3530e5ca33c24599758e5b17a1662
MadMaxINDIAN <naman22khater@gmail.com>
2022-10-02 11:26:25 +0530

    delete image from aws s3

--- controllers/product.category.deleteCategory.js
+++ controllers/product.category.deleteCategory.js
@@ -7,14 +7,13 @@ exports.deleteCategory = async (req, res) => {
         if (!category) {
             return res.status(404).json({ message: 'Category does not exist' })
         }
-        // TODO
         // Delete image from AWS
-        // const imageUrl = category.imageUrl
-        // let key = imageUrl.split('/')[3]
-        // key = key.replaceAll("%2C", ",")
-        // await s3
-        // .deleteObject({ Bucket: "bloomdev", Key: key })
-        // .promise();
+        const imageUrl = category.imageUrl
+        let key = imageUrl.split('/')[3]
+        key = key.replaceAll("%2C", ",")
+        await s3
+        .deleteObject({ Bucket: "bloomdev", Key: key })
+        .promise();
         res.status(200).json({ message: 'Category deleted' })
     } catch (err) {
         console.log(err)
