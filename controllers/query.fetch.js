1b302f7fe869b7b078afeb07c2ad9e50d8ee7279
Samyak Shah <samyakshah123@gmail.com>
2022-12-27 21:15:20 +0530

    Fixed a bug

--- controllers/query.fetch.js
+++ controllers/query.fetch.js
@@ -7,9 +7,9 @@ exports.fetchTotalQueries = async (req, res) => {
         return res.status(401).json({ message: 'Not authorized!' })
     }
     try {
-        const collabQueries = await CollaborateQueries.find().count()
-        const exchangeQueries = await ExchangeQueries.find().count()
-        const contactQueries = await ContactQueries.find().count()
+        const collabQueries = await CollaborateQueries.find({ status: 'Pending' }).count()
+        const exchangeQueries = await ExchangeQueries.find({ status: 'Pending' }).count()
+        const contactQueries = await ContactQueries.find({ $or: [{ status: 'In Queue' }, { status: 'Pending' }] }).count()
         return res.status(200).json({ message: 'Queries count fetched successfully', collabQueries, exchangeQueries, contactQueries })
     } catch (err) {
         console.log(err)
